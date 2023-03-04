42a751cfd92b78fe128e1acd6edef096f4686e2e
ruijadom <ruijadomingues@gmail.com>
2023-03-04 19:31:33 +0000

    chore(tailwind): add typography and daisyui plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,10 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+   require("@tailwindcss/typography"), 
+   require("daisyui")
+  ],
 };
 
 module.exports = config;
