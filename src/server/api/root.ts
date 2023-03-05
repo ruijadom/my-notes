a4ab0c8957b1e461e3f9bccb128c14cb607fb929
ruijadom <ruijadomingues@gmail.com>
2023-03-05 12:00:03 +0000

    refactor(trpc): rm route example

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { topicRouter } from "./routers/topic";
 import { noteRouter } from "./routers/note";
 
@@ -9,7 +8,6 @@ import { noteRouter } from "./routers/note";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   topic: topicRouter,
   note: noteRouter,
 });
